1aaaabeac58616a5dda852f1732cbe8b871ad7f6
Aimedin Chorobaev <eliothab58@gmail.com>
2023-08-09 17:50:48 +0600

    solve 5

--- wrappers/Task5.compile.ts
+++ wrappers/Task5.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton-community/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/imports/stdlib.fc', 'contracts/5.fc'],
+    targets: ['contracts/imports/stdlib.fc', 'contracts/task5.fc'],
 };
